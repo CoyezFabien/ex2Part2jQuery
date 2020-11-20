06c287d6f8035c3ab3229d0d06ef5176999d1912
CoyezFabien <coyez.fabien@gmail.com>
2020-11-20 17:44:54 +0100

    ex2part2jQuery

---
+++ assets/js/exercice2.js
@@ -0,0 +1,8 @@
+// Au clic sur le bouton, afficher une boîte de dialogue avec le message de votre choix.
+$(function(){
+$('#clickMe').click(function()
+{
+alert('je suis une alerte');
+})
+});
+// sert à indiquer qu' une fois la page chargger totalement , le navigateur peut interpreter le code jQuery
